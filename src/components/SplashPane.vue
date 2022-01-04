a482d41a42a9161e28011b8b03b4e7dc05bbcfdd
Lester Huang <huangl302d@gmail.com>
2022-01-03 22:37:11 -0500

    add a picture of myself, may modify depending on derp score

--- src/components/SplashPane.vue
+++ src/components/SplashPane.vue
@@ -3,10 +3,12 @@
 
 <template>
     <div id="splash" class="pane">
-        <h1>👋 I'm Lester Huang</h1>
-        <h5>A lifelong learner 📖 with a desire for adventure 🗺️</h5>
-        <div id="splash-blurb">
-            <p>When I was a kid, I was very picky and scared to try new things (especially food), so to recoup all those lost experiences, I aspire to seek discomfort in every aspect of my life</p>
+        <div id="splash-text">
+            <h1>👋 I'm Lester Huang</h1>
+            <h5>A lifelong learner 📖 with a desire for adventure 🗺️</h5>
+            <div id="splash-blurb">
+                <p>When I was a kid, I was very picky and scared to try new things (especially food), so to recoup all those lost experiences, I aspire to seek discomfort in every aspect of my life</p>
+            </div>
         </div>
     </div>
 </template>
@@ -15,23 +17,32 @@
 <style scoped lang="scss">
 #splash {
     background: #ffa86d;
-    min-height: 50vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    background-image: url('cartoon.png');
+    background-repeat: no-repeat;
+    background-position: right bottom;
+    background-size: 20rem;
+    height: 50vh;
+    
+    #splash-text {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 3rem;
+        margin-right: 20rem;
 
-    #splash-blurb {
-        width: 30rem;
-    }
-
-    h1 {
-    margin-top: 10vh;
-    margin-bottom: 2rem;
-    }
-
-    h5 {
-        margin-top: 0;
-        margin-bottom: 1rem;
+        #splash-blurb {
+            width: 30rem;
+        }
+    
+        h1 {
+        margin-top: 10vh;
+        margin-bottom: 2rem;
+        }
+    
+        h5 {
+            margin-top: 0;
+            margin-bottom: 1rem;
+        }
     }
 }
 </style>
