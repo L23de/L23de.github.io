a329ae15684718ba1ab9a8f348dfd6c605c812d4
Lester Huang <huangl302d@gmail.com>
2022-01-03 21:13:28 -0500

    add short blurb

---
+++ src/components/InterestPane.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+    <div id="hobbies" class="pane">
+        <h2 class="header">My interests...</h2>
+        <!-- Brief resume -->
+    </div>
+</template>
+
+
+<style scoped lang="scss">
+#hobbies {
+    background: $accent;
+    color: white;
+}
+</style>
--- src/components/SplashPane.vue
+++ src/components/SplashPane.vue
@@ -4,8 +4,10 @@
 <template>
     <div id="splash" class="pane">
         <h1>👋 I'm Lester Huang</h1>
-        <p>Short blurb about me!</p>
-        <!-- Brief about me with a good picture -->
+        <h5>A lifelong learner 📖 with a desire for adventure 🗺️</h5>
+        <div id="splash-blurb">
+            <p>When I was a kid, I was very picky and scared to try new things (especially food), so to recoup all those lost experiences, I aspire to seek discomfort in every aspect of my life</p>
+        </div>
     </div>
 </template>
 
@@ -14,5 +16,22 @@
 #splash {
     background: #ffa86d;
     min-height: 60vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    #splash-blurb {
+        width: 30rem;
+    }
+
+    h1 {
+    margin-top: 15vh;
+    margin-bottom: 2rem;
+    }
+
+    h5 {
+        margin-top: 0;
+        margin-bottom: 1rem;
+    }
 }
 </style>
