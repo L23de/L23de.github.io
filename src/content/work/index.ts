1a3992d652582b1c13f58edfb5294d810cf7ebbe
Lester Huang <huangl302d@gmail.com>
2023-07-27 18:14:45 -0600

    update for lazarus

--- src/content/work/index.ts
+++ src/content/work/index.ts
@@ -3,8 +3,9 @@ import Lehigh from './lehigh.json';
 import Millennium from './mm.json';
 import Optum from './optum.json';
 import Kodak from './kodak.json';
+import Lazarus from './lazarus.json';
 
-// JSON Order listed in reverse choronological order (Most recent to oldest)
-const workData = [Kodak, Millennium, Lehigh, Optum, JHU];
+// JSON Order listed in reverse chronological order (Most recent to oldest)
+const workData = [Lazarus, Kodak, Millennium, Lehigh, Optum, JHU];
 
 export default workData;
